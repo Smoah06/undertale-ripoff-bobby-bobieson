7f7cb7494b844252e92a2afd9ec06ef6fac277f0
Smoah06 <smoah2.0@gmail.com>
2021-09-30 14:58:51 +0800

    unity gayme ebic

---
+++ Assets/script/battleMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class battleMovement : MonoBehaviour
+{
+    float speed = 3.5f;
+
+    void Update()
+    {
+
+        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime * speed));
+
+    }
+}
